01e1df6d6fd845a3c84921ab649928cd9d15a7ac
Chunghun Ha <chunghun.ha@gmail.com>
2024-03-03 10:10:33 +0900

    Update my_1st_st.py

--- my_1st_st.py
+++ my_1st_st.py
@@ -2,6 +2,7 @@ import streamlit as st  # streamlit 라이브러리 임포트
 
 # 타이틀 텍스트 출력
 st.title('첫번째 웹 어플 만들기 👋📺')
+st.title('첫번째 웹 어플 만들기 👋📺 이후로 수정한 것입니다. 🌃🌍🤵𝕃𝕆ℕ𝔼𝕎𝕆𝕃𝔽')
 
 # 텍스트 출력
 st.write('# 1. Markdown 텍스트 작성하기')
